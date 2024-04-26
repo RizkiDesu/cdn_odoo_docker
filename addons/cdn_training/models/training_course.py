aaaccab832ae882c76804614bea63345db880a5d
Rizki Desu <s4y4pu1an6@gmail.com>
2024-04-26 15:44:20 +0700

    Membuat oe_chatter dan Tracking perubahan data

--- addons/cdn_training/models/training_course.py
+++ addons/cdn_training/models/training_course.py
@@ -18,26 +18,27 @@ class TrainingCourse(models.Model):
 class TrainingSession(models.Model):
     _name               = 'training.session'
     _description        = 'Training Session'
+    _inherit            = ['mail.thread', 'mail.activity.mixin']
 
-    name                = fields.Char(string='Sesi Trainig', required=True)
-    course_id           = fields.Many2one(comodel_name='training.course', string='Nama Kursus', required=True)
-    start_date          = fields.Date(string='Tanggal Mulai', required=True)
-    duration            = fields.Float(string='Durasi (s)', required=True)
-    seats               = fields.Integer(string='MAX Peserta', required=True, default=1)
-    instruktur_id       = fields.Many2one(comodel_name='instruktur', string='Nama Instruktur')
+    name                = fields.Char(string='Sesi Trainig', tracking=True)
+    course_id           = fields.Many2one(comodel_name='training.course', string='Nama Kursus', tracking=True)
+    start_date          = fields.Date(string='Tanggal Mulai', tracking=True)
+    duration            = fields.Float(string='Durasi (s)', tracking=True)
+    seats               = fields.Integer(string='MAX Peserta', default=1, tracking=True)
+    instruktur_id       = fields.Many2one(comodel_name='instruktur', string='Nama Instruktur', tracking=True)
     
-    alamat              = fields.Char(string='Alamat', related='instruktur_id.street')
-    no_hp               = fields.Char(string='No Hp', related='instruktur_id.mobile')
-    email               = fields.Char(string='Email', related='instruktur_id.email')
-    jenis_kel           = fields.Selection(string='Jenis Kelamin', related='instruktur_id.jenis_kel')
+    alamat              = fields.Char(string='Alamat', related='instruktur_id.street', tracking=True)
+    no_hp               = fields.Char(string='No Hp', related='instruktur_id.mobile', tracking=True)
+    email               = fields.Char(string='Email', related='instruktur_id.email', tracking=True)
+    jenis_kel           = fields.Selection(string='Jenis Kelamin', related='instruktur_id.jenis_kel', tracking=True)
 
-    peserta_ids = fields.Many2many(comodel_name='peserta', string='Peserta')
+    peserta_ids = fields.Many2many(comodel_name='peserta', string='Peserta', tracking=True)
 
     # oocom 
-    jml_peserta = fields.Char(compute='_compute_jml_peserta', string='jml_peserta')
+    jml_peserta = fields.Char(compute='_compute_jml_peserta', string='jml_peserta', tracking=True)
     
     
-    state = fields.Selection(string='Status', selection=[('draf', 'Draft'), ('progres', 'Sedang Berlangsung'),('done', 'Selesai')], default='draf')
+    state = fields.Selection(string='Status', selection=[('draf', 'Draft'), ('progres', 'Sedang Berlangsung'),('done', 'Selesai')], default='draf', tracking=True)
 
 
     @api.depends('peserta_ids')
