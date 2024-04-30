c212e49410e224eb6bd543ae215fccb8f72d2a0f
RIZKI DESU <s4y4pu1an6@gmail.com>
2024-05-01 03:12:02 +0700

    18. How To Enable Tracking For Fields In Odoo || Track Field Changes In Odoo || Audit Log Odoo

--- addons/om_hospital/models/patient.py
+++ addons/om_hospital/models/patient.py
@@ -6,12 +6,12 @@ class CdnPasien(models.Model):
     _description = 'Cdn Pasien'
     _inherit = ['mail.thread','mail.activity.mixin']
 
-    name = fields.Char(string='Nama Pasien')
+    name = fields.Char(string='Nama Pasien', tracking=True)
     ref = fields.Char(string='Refrensi')
-    umur = fields.Integer(string='Umur')
+    umur = fields.Integer(string='Umur', tracking=True)
     jenis_kel = fields.Selection(string='Jenis Kelamin', 
                                  selection=[('l', 'laki laki'), 
-                                            ('p', 'perempuan'),])
+                                            ('p', 'perempuan'),], tracking=True)
     active = fields.Boolean(string='Aktif', default=True)
     
 
