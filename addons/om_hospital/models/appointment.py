bdec582b9105c694562e245724888ab8d1cc602a
RIZKI DESU <s4y4pu1an6@gmail.com>
2024-05-01 09:16:19 +0700

    22. How To Set Default Values For Fields In Odoo || Default Field Values || Odoo 15 Development

--- addons/om_hospital/models/appointment.py
+++ addons/om_hospital/models/appointment.py
@@ -4,10 +4,11 @@ class CdnAppointment(models.Model):
     _name = 'cdn.appointment'
     _description = 'Cdn Appointment'
 
-    
+
     pasien_id = fields.Many2one(comodel_name='cdn.pasien', string='Pasien')
-    appoinment_time = fields.Date(string='Appoinment Time')
-    booking_date = fields.Date(string='Tanggal Booking')
+    appoinment_time = fields.Datetime(string='Appoinment Time', default=fields.Datetime.now)
+    
+    booking_date = fields.Date(string='Tanggal Booking', default=fields.Date.context_today)
     
     
     
--- addons/om_hospital/models/patient.py
+++ addons/om_hospital/models/patient.py
@@ -7,11 +7,14 @@ class CdnPasien(models.Model):
     _inherit = ['mail.thread','mail.activity.mixin']
 
     name = fields.Char(string='Nama Pasien', tracking=True)
-    ref = fields.Char(string='Refrensi')
+    ref = fields.Char(string='Refrensi', default='Odoo Mates')
     umur = fields.Integer(string='Umur', tracking=True)
     jenis_kel = fields.Selection(string='Jenis Kelamin', 
                                  selection=[('l', 'laki laki'), 
-                                            ('p', 'perempuan'),], tracking=True)
+                                            ('p', 'perempuan'),], 
+                                            tracking=True,
+                                            default='p')
+    
     active = fields.Boolean(string='Aktif', default=True)
     
 
