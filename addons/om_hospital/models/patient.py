05bef493d29d566ec46ca98ee6b64e4562d2b85d
RIZKI DESU <s4y4pu1an6@gmail.com>
2024-05-01 11:22:52 +0700

    23. How To Add Related Fields In Odoo || Relational Fields In Odoo || Odoo 15 Tutorials and 24. How To Create Computed Field In Odoo 15 || Compute Field In Odoo 15 || Odoo 15 Development

--- addons/om_hospital/models/patient.py
+++ addons/om_hospital/models/patient.py
@@ -1,14 +1,27 @@
 from odoo import api, fields, models
-
+from datetime import date
 
 class CdnPasien(models.Model):
-    _name = 'cdn.pasien'
-    _description = 'Cdn Pasien'
-    _inherit = ['mail.thread','mail.activity.mixin']
+    _name           = 'cdn.pasien'
+    _description    = 'Cdn Pasien'
+    _inherit        = ['mail.thread','mail.activity.mixin']
 
-    name = fields.Char(string='Nama Pasien', tracking=True)
-    ref = fields.Char(string='Refrensi', default='Odoo Mates')
-    umur = fields.Integer(string='Umur', tracking=True)
+    name            = fields.Char(string='Nama Pasien', tracking=True)
+    tgl_lahir       = fields.Date(string='Tangggal Lahir')
+    ref             = fields.Char(string='Refrensi', default='Odoo Mates')
+
+    umur            = fields.Integer(string='Umur', tracking=True, compute='_compute_umur')
+    
+    @api.depends('tgl_lahir')
+    def _compute_umur(self):
+        for rec in self:
+            today = date.today()
+            if rec.tgl_lahir:
+                rec.umur = today.year - rec.tgl_lahir.year
+            else:
+                rec.umur = 0
+    
+    
     jenis_kel = fields.Selection(string='Jenis Kelamin', 
                                  selection=[('l', 'laki laki'), 
                                             ('p', 'perempuan'),], 
