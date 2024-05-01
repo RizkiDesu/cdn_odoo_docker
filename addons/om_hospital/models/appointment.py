b982e7bb5b145df41ff5903ed84911001ea20b1a
RIZKI DESU <s4y4pu1an6@gmail.com>
2024-05-01 08:53:45 +0700

    21. How To Add Date And Datetime Fields In Odoo || Odoo 15 Tutorials

--- addons/om_hospital/models/appointment.py
+++ addons/om_hospital/models/appointment.py
@@ -4,5 +4,10 @@ class CdnAppointment(models.Model):
     _name = 'cdn.appointment'
     _description = 'Cdn Appointment'
 
+    
     pasien_id = fields.Many2one(comodel_name='cdn.pasien', string='Pasien')
+    appoinment_time = fields.Date(string='Appoinment Time')
+    booking_date = fields.Date(string='Tanggal Booking')
+    
+    
     
