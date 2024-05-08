f2a58acffeaa142b0f88f2b2617952627fae7987
RIZKI DESU <s4y4pu1an6@gmail.com>
2024-05-08 09:05:46 +0700

    61. Transient Model In Odoo || Odoo 15 Development Tutorials || Odoo Advanced Tutorials

---
+++ addons/om_hospital/wizards/batal_appointment.py
@@ -0,0 +1,10 @@
+from odoo import api, fields, models
+
+
+class CdnWzBatalapointment(models.TransientModel):
+    _name = 'cdn.wz.batalapointment'
+    _description = 'Cdn Wz Batalapointment'
+
+    appointment_id = fields.Many2one(comodel_name='cdn.pasien', string='Appointment')
+    
+
