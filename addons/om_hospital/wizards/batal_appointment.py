fa24de2973a492af13765bd1ecc3f0f2c1ec36cf
RIZKI DESU <s4y4pu1an6@gmail.com>
2024-05-12 00:23:14 +0700

    74. Default Get Function In Odoo || Odoo Default Get Method || Odoo 15 Tutorial || Odoo ORM Methods

--- addons/om_hospital/wizards/batal_appointment.py
+++ addons/om_hospital/wizards/batal_appointment.py
@@ -1,5 +1,5 @@
 from odoo import api, fields, models
-
+import datetime as tanggal
 
 class CdnWzBatalapointment(models.TransientModel):
     _name           = 'cdn.wz.batalapointment'
@@ -7,9 +7,18 @@ class CdnWzBatalapointment(models.TransientModel):
 
     appointment_id  = fields.Many2one(comodel_name='cdn.pasien', string='Appointment')
 
-    reason          = fields.Text(string='Reason')
+    reason          = fields.Text(string='Reason') 
+    tanggal_batal = fields.Date(string='Tanggal Batal')
     
-
+    
+    @api.model
+    def default_get(self, fields):
+        ref = super (CdnWzBatalapointment, self).default_get(fields)
+        print("default get executed", ref)
+        ref['tanggal_batal'] = tanggal.date.today()
+        # ref['tanggal_batal'] = fields.Date.context_today(self) # ora iso
+        return ref
+    
     def action_cancel(self):
         return
     
