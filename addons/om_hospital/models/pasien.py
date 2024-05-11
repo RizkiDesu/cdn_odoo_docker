5a7912ee251dc37eda8eeeaa817de1d27cf69673
RIZKI DESU <s4y4pu1an6@gmail.com>
2024-05-11 20:54:17 +0700

    71. How To Override Create Method In Odoo || Odoo 15 Tutorial || Odoo ORM Methods

--- addons/om_hospital/models/pasien.py
+++ addons/om_hospital/models/pasien.py
@@ -13,15 +13,6 @@ class CdnPasien(models.Model):
 
     umur            = fields.Integer(string='Umur', tracking=True, compute='_compute_umur')  # field umur dengan fungsi compute _compute_umur untuk menghitung umur pasien berdasarkan tanggal lahir pasien
     
-    @api.depends('tgl_lahir') #fungsi untuk menghitung umur pasien berdasarkan tanggal lahir pasien 
-    def _compute_umur(self):
-        for rec in self: #looping untuk menghitung umur pasien 
-            today = date.today() #mengambil tanggal hari ini
-            if rec.tgl_lahir: #jika tanggal lahir pasien ada
-                rec.umur = today.year - rec.tgl_lahir.year # menghitung umur pasien
-            else:
-                rec.umur = 0 #jika tidak di kasih else nilai bakal null dan error
-    
     jenis_kel       = fields.Selection(string='Jenis Kelamin', 
                                  selection=[('l', 'laki laki'), 
                                             ('p', 'perempuan'),], 
@@ -33,6 +24,24 @@ class CdnPasien(models.Model):
     appoinment_id   = fields.Many2one(comodel_name='cdn.appointment', string='Pasien') # many to one ke tabel cdn appointment
     image           = fields.Image(string='gambar') # field image
     tag_ids         = fields.Many2many(comodel_name='cdn.pasien.tag', string='Tags') # many to many ke tabel cdn pasien tag
+
+    @api.model
+    def create (self, vals): # fungsi create untuk membuat record baru
+        print("odoo mates")
+        # contoh isi adalah vals = {'__last_update': False, 'image': False, 'name': 'nama pasien', 'tgl_lahir': '2024-05-07', 'appoinment_id': 14, 'ref': 'Odoo Mates', 'jenis_kel': 'l', 'tag_ids': [[6, False, [18]]], 'active': True}
+        
+        vals['ref'] = vals['ref'] or 'Odoo Mates' # jika ref tidak di isi maka ref akan di isi dengan Odoo Mates
+        # vals['ref'] = 'Odoo Mates' # mengisi ref dengan Odoo Mates
+        return super(CdnPasien, self).create(vals) # membuat record baru dengan nilai vals yang di inputkan
+
+    @api.depends('tgl_lahir') #fungsi untuk menghitung umur pasien berdasarkan tanggal lahir pasien 
+    def _compute_umur(self):
+        for rec in self: #looping untuk menghitung umur pasien 
+            today = date.today() #mengambil tanggal hari ini
+            if rec.tgl_lahir: #jika tanggal lahir pasien ada
+                rec.umur = today.year - rec.tgl_lahir.year # menghitung umur pasien
+            else:
+                rec.umur = 0 #jika tidak di kasih else nilai bakal null dan error
     
     
     
