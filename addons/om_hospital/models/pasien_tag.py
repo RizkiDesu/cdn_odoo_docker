b29344bcca932abd7ce439a37f2339e073df9375
RIZKI DESU <s4y4pu1an6@gmail.com>
2024-05-13 04:46:46 +0700

    84. Copy Function In Odoo | Copy Attribute In Odoo | Odoo Copy ORM Method

--- addons/om_hospital/models/pasien_tag.py
+++ addons/om_hospital/models/pasien_tag.py
@@ -1,4 +1,4 @@
-from odoo import api, fields, models
+from odoo import api, fields, models,_
 
 
 class CdnPasienTag(models.Model):
@@ -9,15 +9,20 @@ class CdnPasienTag(models.Model):
     active          = fields.Boolean(string='Aktif', default=True) # field active
     warna           = fields.Integer(string='Warna') # field warna
     warna2          = fields.Char(string='Warna 2') # field warna 2
-    sequence        = fields.Integer(string='Sequence') # field sequence
+    sequence        = fields.Integer(string='Sequence', default=1) # field sequence
 
+    @api.returns('self', lambda value: value.id) # fungsi untuk mengembalikan nilai id
+    def copy(self, default=None): # fungsi untuk mengcopy data
+        if default is None: # jika default kosong
+            default = {} # default di isi dengan dictionary kosong
+        if not default.get('name'): # jika tidak ada nama
+            default['name'] = _("%s (copy)") % (self.name) # nama di isi dengan nama copy
+        default['sequence'] = 10
+        return super(CdnPasienTag, self).copy(default) # copy data
+    
     _sql_constraints = [ # constraint
         # ('unique_tag_name', 'unique(name, active)', 'Nama Harus Unik'), 
         ('unique_tag_name', 'unique(name)', 'Nama Harus Unik'), 
         ('chek_sequence', 'check(sequence > 0)', 'Sequence tidak boleh angka dibawah 0') 
     ]
-
-    # _sql_constraints    = [
-    #     ("name_course_unique", "UNIQUE(name)", "Nama Kursus Sudah Ada"),
-    # ]
     
