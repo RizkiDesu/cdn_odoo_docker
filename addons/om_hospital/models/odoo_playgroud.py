b33518263a49dfb2add5f0d44929a5338a1b9481
RIZKI DESU <s4y4pu1an6@gmail.com>
2024-05-12 08:33:39 +0700

    77. Target Inline In Odoo || Inline Actions In Odoo || Target In Odoo Actions || Odoo Window Action

--- addons/om_hospital/models/odoo_playgroud.py
+++ addons/om_hospital/models/odoo_playgroud.py
@@ -1,28 +1,28 @@
 from odoo import api, fields, models
-from odoo.tools.safe_eval import safe_eval
+from odoo.tools.safe_eval import safe_eval # digunakan untuk menjalankan code python nanti
 
 class OdooPlaygroud(models.Model):
     _name = 'odoo.playgroud'
     _description = 'Odoo Playgroud'
 
-    DEFAULT_ENV_VARIABLES = """ # sumary_line
+    DEFAULT_DATA_CODE = """ 
+    # sumary_line
     # input your code here
     # in line 
     # for example
     # print('Hello World')
-
     """
     
     model_id = fields.Many2one(comodel_name='ir.model', string='Model')
-    code = fields.Text(string='Code', default=DEFAULT_ENV_VARIABLES)
+    code = fields.Text(string='Code', default = DEFAULT_DATA_CODE)
     result = fields.Text(string='Result')
 
     def action_execute(self):
-        try:
-            if self.model_id:
-                model = self.env[self.model_id.model]
+        try: # mencoba menjalankan code
+            if self.model_id: # jika model id ada
+                model = self.env[self.model_id.model] 
             else:
-                model = self
-            self.result = safe_eval(self.code, {'self': self, 'model': model})
-        except Exception as e:
-            self.result = str(e)
+                model = self #
+            self.result = safe_eval(self.code, {'self': self, 'model': model}) # menjalankan code
+        except Exception as e: # jika terjadi error
+            self.result = str(e) # pesan error jika terjadi error
