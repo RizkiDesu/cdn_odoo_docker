7f2b8fa3c2531f6d9406616240d4589f776153ac
RIZKI DESU <s4y4pu1an6@gmail.com>
2024-05-15 14:12:36 +0700

    105 How To Access Value From Settings In Odoo || Odoo Module Settings

--- addons/om_hospital/wizards/batal_appointment.py
+++ addons/om_hospital/wizards/batal_appointment.py
@@ -1,6 +1,7 @@
+import datetime
 from odoo import api, fields, models, _
-import datetime as tanggal
-
+from dateutil import relativedelta
+from datetime import date 
 from odoo.exceptions import ValidationError as pesan_error
 
 class CdnWzBatalapointment(models.TransientModel):
@@ -20,17 +21,28 @@ class CdnWzBatalapointment(models.TransientModel):
     @api.model
     def default_get(self, fields): 
         ref = super (CdnWzBatalapointment, self).default_get(fields)
-        ref['tanggal_batal'] = tanggal.date.today() 
+        ref['tanggal_batal'] = datetime.date.today() 
         ref['reason'] = 'Alasan Batal py'
-        print("............", self.env.context)
+        # print("............", self.env.context)
         # hasil ............{'lang': 'en_US', 'tz': 'Asia/Jakarta', 'uid': 2, 'allowed_company_ids': [1], 'active_model': 'cdn.appointment', 'active_id': 24, 'active_ids': [24]}
         if self.env.context.get('active_id'): # jika active id ada
             ref['app_id'] = self.env.context.get('active_id') # mengambil active id
         return ref
     
+
+    # fungsi 
     def action_cancel(self): # fungsi cancel untuk mengubah state menjadi cancel
-        if self.app_id.booking_date == fields.Date.today(): # jika tanggal booking sama dengan hari ini
-            raise pesan_error(_("Tidak bisa membatalkan appoinment hari ini"))
+        batal_day = self.env['ir.config_parameter'].get_param('om_hospital.hari_batal') # mengambil nilai dari parameter hari batal
+        allowed_date = self.app_id.booking_date - relativedelta.relativedelta(days=int(batal_day))
+
+        print (allowed_date, "<", date.today())
+        print (allowed_date < date.today())
+        # fungsi ini untuk membatalkan appoinment jika tanggal batal lebih kecil dari hari ini maka tampilkan pesan error
+        # contoh kasus di kehidupan nyata jika kita ingin membatalkan appoinment kita harus memberitahu dokter atau rumah sakit
+        # minimal 1 hari sebelumnya jika tidak maka akan dikenakan denda
+        if allowed_date < date.today(): # jika tanggal batal lebih kecil dari hari ini maka tampilkan pesan error
+            raise pesan_error(_("Tidak bisa membatalkan appoinment karena sudah melewati batas hari batal"))
+
         self.app_id.state = 'cancel' # mengubah state menjadi cancel
-        return
+        # return
     
